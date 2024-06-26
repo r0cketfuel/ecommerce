4be73fa050fbd0c89fa1a97a8e8dd548645c2cda
fpizzuti <fernando_p405@hotmail.com>
2024-06-26 09:00:42 -0300

    Script para deploy en servidor

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Clonación del repositorio
+git clone https://github.com//ecommerce.git
+
+# Navegación al directorio del proyecto
+cd /home/fernand1/ecommerce
+
+# Renombrado del archivo .env
+mv .env.example .env
+
+# Instalación de dependencias de Composer
+composer install --prefer-dist --no-dev -o
+
+# Ejecución de migraciones y seeds de Laravel
+php artisan migrate && php artisan db:seed
+
+# Optimización de la aplicación
+php artisan optimize
+
+# Mensaje de finalización
+echo "Despliegue realizado con éxito!"
